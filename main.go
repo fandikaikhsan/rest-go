fd98fe9155871f6cd962e26ebbff40afca08c36c
Fandika <fandika.ikhsan@gmail.com>
2023-04-18 02:18:49 +0700

    feat: compile get db to getalbum service

--- main.go
+++ main.go
@@ -1,44 +1,14 @@
 package main
 
-import (
-	"context"
-	"database/sql"
-	"fmt"
-	"os"
+import "github.com/gin-gonic/gin"
 
-	"github.com/joho/godotenv"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
-)
+func main() {
+	router := gin.Default()
 
-func main () {
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/", getRoot)
 
-	ctx := context.Background()
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-	dsn_url := os.Getenv("DATABASE_URL")
-
-	dsn := dsn_url 
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db := bun.NewDB(sqldb, pgdialect.New())
-
-	album := new(album)
-	if err := db.NewSelect().Model(album).Where("id = ?", 1).Scan(ctx); err != nil { panic(err) }
-
-	fmt.Printf("album: %#v", album)
-	return
+	router.Run("localhost:8080")
 }
-
-// func main() {
-// 	router := gin.Default()
-
-// 	router.GET("/albums", getAlbums)
-// 	router.POST("/albums", postAlbums)
-// 	router.GET("/", getRoot)
-
-// 	router.Run("localhost:8080")
-// }
 
--- rest.go
+++ rest.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
 
 func getAlbums(c *gin.Context) {
-	// album := conn()
-	c.IndentedJSON(http.StatusOK, albums)
+
+	ctx := context.Background()
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	dsn_url := os.Getenv("DATABASE_URL")
+
+	dsn := dsn_url 
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db := bun.NewDB(sqldb, pgdialect.New())
+
+	album := new(album)
+	if err := db.NewSelect().Model(album).Where("id = ?", 1).Scan(ctx); err != nil { panic(err) }
+
+	fmt.Printf("album: %#v", album)
+
+	c.IndentedJSON(http.StatusOK, album)
 	
 }
 
