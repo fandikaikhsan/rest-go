d34986d47675f3a85611990ba06560de9ab592aa
Fandika <fandika.ikhsan@gmail.com>
2023-04-18 01:44:03 +0700

    feat: get env secret

--- main.go
+++ main.go
@@ -4,17 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	// router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/", getRoot)
 
 	router.Run("localhost:8080")
 }
--- rest.go
+++ rest.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func getAlbums(c *gin.Context) {
+func getAlbums(secret, c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
@@ -17,4 +19,13 @@ func postAlbums(c *gin.Context) {
 	}
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getRoot(c *gin.Context) {
+	err := godotenv.Load()
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+	}
+	secret := os.Getenv("SECRET")
+	c.IndentedJSON(http.StatusOK, secret)
 }
