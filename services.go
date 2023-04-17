933bb92d9ea08fd4bed08242a738b2c98dadd2c0
Fandika Ikhsan <fandika.ikhsan@gmail.com>
2023-04-18 03:00:10 +0700

    Feat/redis (#2)
    * feat: init redis connect client
    
    * feat: get key value
    
    * chore: mv url to env

--- services.go
+++ services.go
@@ -9,30 +9,48 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 )
 
+
 func getAlbums(c *gin.Context) {
 
+	// setup database config
 	ctx := context.Background()
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	if err := godotenv.Load(); err != nil { panic(err) }
 	dsn_url := os.Getenv("DATABASE_URL")
 
 	dsn := dsn_url 
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(sqldb, pgdialect.New())
 
+	// setup redis
+
+	rdb_addr := os.Getenv("REDIS_URL")
+	// rdb_pass := os.Getenv("REDIS_PASS")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:    rdb_addr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	if err := rdb.Set(ctx, "key", "value", 0).Err() ; err != nil { panic(err) }
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil { panic(err) }
+
+	fmt.Println("key: ", val)
+
 	album := new(album)
 	if err := db.NewSelect().Model(album).Where("id = ?", 1).Scan(ctx); err != nil { panic(err) }
 
 	fmt.Printf("album: %#v", album)
 
-	c.IndentedJSON(http.StatusOK, album)
+	c.IndentedJSON(http.StatusOK, val)
 	
 }
 
